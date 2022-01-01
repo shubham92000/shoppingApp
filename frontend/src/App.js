98085f0ff74670fdd778e894b1ab7ae60ab5c1e1
shubham92000 <sbhsingh616@gmail.com>
2022-01-01 18:16:46 +0530

    added header and footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,20 @@
 import { Fragment } from 'react';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 const App = () => {
-  return <Fragment className="App">App</Fragment>;
+  return (
+    <Fragment className="App">
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to shopping website</h1>
+        </Container>
+      </main>
+      <Footer />
+    </Fragment>
+  );
 };
 
 export default App;
