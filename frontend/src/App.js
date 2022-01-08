976b624111b2fdb68c902ad6e46d3feeea3fb741
shubham92000 <sbhsingh616@gmail.com>
2022-01-08 07:54:09 +0530

    added ProductScreen , react-router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,25 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import HomeScreen from './components/screens/HomeScreen';
+import ProductScreen from './components/screens/ProductScreen';
 
 const App = () => {
   return (
-    <div className="App">
-      <Header />
-      <main className="py-3">
-        <Container>
-          <HomeScreen />
+    <Router>
+      <div className="App">
+        <Header />
+        <Container className="py-3">
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+            <Route path="/product/:id" element={<ProductScreen />} />
+          </Routes>
         </Container>
-      </main>
-      <Footer />
-    </div>
+        <Footer />
+      </div>
+    </Router>
   );
 };
 
