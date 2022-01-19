48ccfda87d8a3516d9ead34560a8a950f50974c4
shubham92000 <sbhsingh616@gmail.com>
2022-01-19 19:56:42 +0530

    added colors

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,9 +5,9 @@ const connectDB = async () => {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
     });
-    console.log(`MongoDB connected : ${conn.connection.host} `);
+    console.log(`MongoDB connected : ${conn.connection.host} `.cyan.underline);
   } catch (err) {
-    console.log(`Error : ${err.message}`);
+    console.log(`Error : ${err.message}`.red.underline.bold);
     process.exit(1);
   }
 };
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@
 
 import express from 'express';
 import dotenv from 'dotenv';
+import colors from 'colors';
 import connectDB from './config/db.js';
 import products from './data/products.js';
 
@@ -30,5 +31,7 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
