60ff12e66851e38088f9f61ceed393eb89557e90
shubham92000 <sbhsingh616@gmail.com>
2022-03-31 20:05:24 +0530

    added orders  in profile page

--- frontend/src/action/orderActions.js
+++ frontend/src/action/orderActions.js
@@ -9,6 +9,9 @@ import {
   ORDER_PAY_REQUEST,
   ORDER_PAY_SUCCESS,
   ORDER_PAY_FAIL,
+  ORDER_LIST_MY_REQUEST,
+  ORDER_LIST_MY_SUCCESS,
+  ORDER_LIST_MY_FAIL,
 } from '../constants/orderConstants';
 
 const createOrder = (order) => async (dispatch, getState) => {
@@ -116,4 +119,37 @@ const payOrder = (orderId, paymentResult) => async (dispatch, getState) => {
   }
 };
 
-export { createOrder, getOrderDetails, payOrder };
+const listMyOrders = () => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: ORDER_LIST_MY_REQUEST,
+    });
+
+    const {
+      userLogin: { userInfo },
+    } = getState();
+
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+
+    const { data } = await axios.get(`/api/orders/myorders`, config);
+
+    dispatch({
+      type: ORDER_LIST_MY_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: ORDER_LIST_MY_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+
+export { createOrder, getOrderDetails, payOrder, listMyOrders };
