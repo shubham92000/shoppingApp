52b314aa60b88338d08cbcbc4339c90bda8c3de8
shubham92000 <sbhsingh616@gmail.com>
2022-04-15 14:34:06 +0530

    added delete product by id controller

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -24,4 +24,19 @@ const getProductById = asyncHandler(async (req, res) => {
   }
 });
 
-export { getProducts, getProductById };
+// @desc    delete a product
+// @route   DELETE /api/products/:id
+// @access  Public Private/Admin
+const deleteProductById = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    await product.remove();
+    res.json({ message: 'Product removed' });
+  } else {
+    res.status(404);
+    throw new Error('Product Not Found');
+  }
+});
+
+export { getProducts, getProductById, deleteProductById };
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,9 +3,14 @@ const router = express.Router();
 import {
   getProducts,
   getProductById,
+  deleteProductById,
 } from '../controllers/productController.js';
+import { protect, admin } from '../middleware/authMiddleware.js';
 
 router.route('/').get(getProducts);
-router.route('/:id').get(getProductById);
+router
+  .route('/:id')
+  .get(getProductById)
+  .delete(protect, admin, deleteProductById);
 
 export default router;
