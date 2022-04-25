5c34bfb634bde060311950996eb8a13a3596bede
shubham92000 <sbhsingh616@gmail.com>
2022-04-25 14:51:58 +0530

    add review for product in frontend

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -17,6 +17,10 @@ import {
   PRODUCT_UPDATE_SUCCESS,
   PRODUCT_UPDATE_FAIL,
   PRODUCT_UPDATE_RESET,
+  PRODUCT_CREATE_REVIEW_REQUEST,
+  PRODUCT_CREATE_REVIEW_SUCCESS,
+  PRODUCT_CREATE_REVIEW_FAIL,
+  PRODUCT_CREATE_REVIEW_RESET,
 } from '../constants/productConstants';
 
 const initialState = {
@@ -151,10 +155,34 @@ const productUpdateReducer = (state = { product: {} }, action) => {
   }
 };
 
+const productReviewCreateReducer = (state = {}, action) => {
+  switch (action.type) {
+    case PRODUCT_CREATE_REVIEW_REQUEST:
+      return {
+        loading: true,
+      };
+    case PRODUCT_CREATE_REVIEW_SUCCESS:
+      return {
+        loading: false,
+        success: true,
+      };
+    case PRODUCT_CREATE_REVIEW_FAIL:
+      return {
+        loading: false,
+        error: action.payload,
+      };
+    case PRODUCT_CREATE_REVIEW_RESET:
+      return {};
+    default:
+      return state;
+  }
+};
+
 export {
   productListReducer,
   productDetailsReducer,
   productDeleteReducer,
   productCreateReducer,
   productUpdateReducer,
+  productReviewCreateReducer,
 };
